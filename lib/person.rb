5b9fe2e8b49f9ca5071d0e50f56eae500ff37392
comic-engineer-3780 <ethanjosephfournier@gmail.com>
2019-09-17 23:25:18 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,10 @@
 class Person
-  def initialize
-    @name = name
-  end
-  
   def name
     @name
   end
   
   def name=(new_name)
     @name = new_name
-  end
-  
-  def initialize
-    @job = job
   end
   
   def job
